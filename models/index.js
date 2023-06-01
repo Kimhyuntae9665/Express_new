24c58eb1711a635311746eef4e504f8a8fc56c31
Kimhyuntae9665 <Kimhyuntae9665>
2023-06-01 15:25:29 +0900

    Sequelize 모듈은 javascript와 데이터베이스를 연결해 준다

--- models/index.js
+++ models/index.js
@@ -10,7 +10,10 @@ const sequelize = new Sequelize(config.database,config.username,config.password,
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.User = require('./users')(sequelize,Sequelize);
+
+// ? sequelize를 이용해서 sequelize : 데이터베이스의 아이디 , 데이터베이스, 아이디,비번을 설정하는 거고 
+// ? Sequelize를 이용해서 Sequelize : Sequelize 모듈을 사용해서 데이터베이스와 자바스크립트의 객체를 Mapping 해 준다
+db.User = require('./user.js')(sequelize,Sequelize);
 db.Comment = require('./comment')(sequelize,Sequelize);
 
 db.User.hasMany(db.Comment,{foreignKey:'commenter',sourceKey:'id'});
